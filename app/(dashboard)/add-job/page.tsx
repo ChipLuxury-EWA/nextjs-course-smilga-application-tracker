4e47951ff3fa0910a778f68447be78c61ce6b201
ChipLuxury-EWA <chip.luxury.ewa@gmail.com>
2024-10-20 04:35:25 +0300

    added basic dashboards pages

---
+++ app/(dashboard)/add-job/page.tsx
@@ -0,0 +1,4 @@
+const AddJobPage = () => {
+  return <h1 className="text-4xl">add jobs </h1>;
+};
+export default AddJobPage;
---
+++ app/(dashboard)/jobs/page.tsx
@@ -0,0 +1,4 @@
+const JobsPage = () => {
+  return <h1 className="text-4xl">jobs page</h1>;
+};
+export default JobsPage;
