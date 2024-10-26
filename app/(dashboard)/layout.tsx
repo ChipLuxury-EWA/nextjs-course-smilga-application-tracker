8c0b7eadaa159f5e0d9c0d5ab3f90b015c67d274
ChipLuxury-EWA <chip.luxury.ewa@gmail.com>
2024-10-26 03:41:30 +0300

    added basic layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,18 @@
-const layout = ({ children }: {children: React.ReactNode}) => {
-  return <div>{children}</div>;
+import { PropsWithChildren } from "react";
+import NavBar from "./_components/NavBar";
+import SideBar from "./_components/SideBar";
+
+const layout = ({ children }: PropsWithChildren) => {
+  return (
+    <main className="grid lg:grid-cols-5">
+      <div className="hidden lg:block lg:col-span-1 lg:min-h-screen">
+        <SideBar />
+      </div>
+      <div className="lg:col-span-4 ">
+        <NavBar />
+        <div className="py-16 px-4 sm:px-8 lg:px-16">{children}</div>
+      </div>
+    </main>
+  );
 };
 export default layout;
---
+++ app/_components/Links.tsx
@@ -0,0 +1,30 @@
+import { Layers, AppWindow, AreaChart } from "lucide-react";
+
+type NavLink = {
+  href: string;
+  label: string;
+  icon: React.ReactNode;
+};
+
+const links: NavLink[] = [
+  {
+    href: "/add-job",
+    label: "Add job",
+    icon: <Layers />,
+  },
+  {
+    href: "/jobs",
+    label: "Jobs",
+    icon: <AppWindow />,
+  },
+  {
+    href: "/stats",
+    label: "Stats",
+    icon: <AreaChart />,
+  },
+];
+
+const Links = () => {
+  return <div>Links</div>;
+};
+export default Links;
